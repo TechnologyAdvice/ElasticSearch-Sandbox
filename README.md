0d0e8dd544b6d29b66629dd49686689a5ca905b0
fluidbyte <kent@fluidbyte.net>
2016-09-20 11:21:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ElasticSearch Sandbox
+
+Allows for sandboxing ElasticSearch testing. Includes Kibana for UI.
+
+Run the turd: 
+
+1. `lab start` will drop you in the container with ES and Kib running
+2. `node index` will start seeding based on the `index.js` script on a random interval
---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict'
+const rp = require('request-promise')
+
+const index = 'test'
+const type = 'log'
+const endpoint = `http://${process.env.ES_KIBANA_PORT_9200_TCP_ADDR}:9200/${index}/${type}/`
+const mock = {
+  statuses: [ 'info', 'warn', 'error' ],
+  systems: [ 'red', 'green', 'blue' ],
+  messages: [
+    'perspiciatis unde omnis iste natus error sit',
+    'fugit, sed quia consequuntur magn',
+    'sed quia non numquam eius modi tempora',
+    'uis autem vel eum iure reprehenderit qui in ea',
+    'non numquam eius modi tempora incidunt'
+  ]
+}
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * max) + min
+}
+const genMockPayload = () => {
+  return {
+    timestamp: +new Date(),
+    status: mock.statuses[getRandomInt(0, mock.statuses.length)],
+    system: mock.systems[getRandomInt(0, mock.systems.length)],
+    message: {
+      content: mock.messages[getRandomInt(0, mock.messages.length)],
+      flag: getRandomInt(1,5)
+    }
+  }
+}
+let timer
+const run = () => {
+  timer = setTimeout(() => {
+    const payload = genMockPayload()
+    rp({
+      method: 'POST',
+      uri: endpoint,
+      body: payload,
+      json: true
+    })
+    .then(res => {
+      console.log('CREATED', res)
+    })
+    .catch(err => {
+      console.log('FAILED', err)
+    })
+    run()
+  }, getRandomInt(1, 1000))
+}
+
+run()
